22514e08d9b69f14b503ada1185021de820b6996
miraclejester <miraclejester413@gmail.com>
2026-03-01 22:09:57 -0500

    melee and projectile sounds

---
+++ systems/audio/local_sfx_manager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+class_name LocalSFXManager
+
+var emitters: Dictionary[String, FmodEventEmitter2D]
+
+func _ready() -> void:
+	for child in get_children():
+		var e: FmodEventEmitter2D = child as FmodEventEmitter2D
+		emitters[e.name] = e
+
+
+func play_sfx(key: String) -> void:
+	emitters[key].play_one_shot()
